b1fe598dd2b43348533eccef0d279bdc8a4ef301
afmjoaa <32086430+afmjoaa@users.noreply.github.com>
2023-10-02 17:22:13 +0600

    #3: Udpate about page text

--- lib/ui/screens/about/about_screen.dart
+++ lib/ui/screens/about/about_screen.dart
@@ -34,34 +34,28 @@ class AboutScreen extends StatelessWidget {
               Text('Demonstrating:',
                   style: TextStyle(fontSize: 16, fontWeight: FontWeight.w900)),
               Text(
-                '1. Wire framing, UX design\n'
-                '2. Clean Architecture, Use case pattern\n'
+                '1. Domain driven design\n'
+                '2. Clean Architecture, Separation of Data, Domain and UI\n'
                 '3. Local data caching\n'
                 '4. Network image caching\n'
-                '5. Inherited widget/provider\n'
+                '5. Inherited widget/provider for loading widget.\n'
                 '6. App routing\n'
                 '7. cubit/BLoC pattern\n'
                 '8. Repository pattern\n'
                 '9. Data flow: UI_LAYER <=> APPLICATION_LAYER(BLoC) <=> DOMAIN_LAYER <=> DATA_LAYER\n'
                 '10. Flare animation loading\n'
-                '11. Error handling\n'
-                '12. Permission handling\n'
-                '13. Pull to refresh',
+                '11. Error handling using dio interceptor\n'
+                '12. Pull to refresh\n'
+                '13. Search using country, category and query\n'
+                '14. Caching search result using hive\n'
+                '14. Using custom fonts\n'
+                '14. Light and Dark theme implementation\n'
+                '14. Beautiful modern UI with animation and sidebar\n'
+                '14. Shimmer if image url is null or empty',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
               ),
               SizedBox(
                 height: 20,
-              ),
-              Text('Improvement Scope:',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w900)),
-              Text(
-                '1. Use Relative imports where-ever possible.\n'
-                '2. Write Unit test.\n'
-                '3. Get all constants like: Color, assets, margin, padding etc from respective constant classes.\n'
-                '4. Rather then reading text file as an map read it as an object\n'
-                '5. Write better dto to serve data to UI layer'
-                '6. Add the dark theme etc.',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
               ),
             ],
           ),
